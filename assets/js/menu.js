901d1109cd08d27d5139906b0811f7a0811de202
Kouta-fd <kotafd0124@icloud.com>
2020-12-21 21:47:31 +0900

    first push

---
+++ assets/js/menu.js
@@ -0,0 +1,8 @@
+function hamburger() {
+  var body = document.body;
+  body.classList.toggle(‘open’);
+  document.getElementById(‘trigger’).classList.toggle(‘active’)
+}
+document.getElementById(‘trigger’).addEventListener(‘click’,function () {
+  hamburger();
+});
