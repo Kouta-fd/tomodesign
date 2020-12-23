18ea1c62aa7d9509047df2fbbaa8d62680c8e5b4
Kouta-fd <kotafd0124@icloud.com>
2020-12-23 14:41:25 +0900

    add content

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,7 +4,7 @@ $(function(){
           var position = $(this).offset().top;
           var scroll = $(window).scrollTop();
           var windowHeight = $(window).height();
-          if (scroll > position - windowHeight + 280){
+          if (scroll > position - windowHeight + 300){
             $(this).addClass('active');
           }
       });
--- assets/js/top.js
+++ assets/js/top.js
@@ -1,9 +1,12 @@
 window.addEventListener('DOMContentLoaded', function(){
 	$(function() {
 		setTimeout(function(){
-			$('.start1').fadeIn(2000);
-		},2000)});
+			$('.start1').fadeIn(1500);
+		},1500)});
 		setTimeout(function(){
-			$('.start2').fadeIn(3000);
+			$('.start2').fadeIn(2500);
+		},2500)
+		setTimeout(function(){
+			$('.start3').fadeIn(3000);
 		},3000)
 });
