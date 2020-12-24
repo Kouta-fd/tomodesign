4bce3a2e3b80710cad2aafbc60eb0f6761501565
Kouta-fd <kotafd0124@icloud.com>
2020-12-24 13:29:42 +0900

    modified

--- assets/js/top.js
+++ assets/js/top.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', function(){
 	$(function() {
 		setTimeout(function(){
 			$('.start1').fadeIn(1500);
-		},1500)});
+		},800)});
 		setTimeout(function(){
 			$('.start2').fadeIn(2500);
-		},2500)
+		},1500)
 		setTimeout(function(){
 			$('.start3').fadeIn(3000);
-		},3000)
+		},2000)
 });
